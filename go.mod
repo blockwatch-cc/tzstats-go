e0cd184e1dfd1acab78e8d6d8d8cf17d141fe4ab
Alexander Eichhorn <alex@blockwatch.cc>
2021-05-02 19:35:54 +0400

    Refactor metadata

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	blockwatch.cc/tzgo v0.9.4
 	github.com/daviddengcn/go-colortext v1.0.0
+	github.com/echa/code v0.0.0-20201118130056-1878364e4ad4
 	github.com/echa/log v1.0.3
 )
--- metadata.go
+++ metadata.go
@@ -5,33 +5,168 @@ package tzstats
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"time"
 
 	"blockwatch.cc/tzgo/tezos"
+	"github.com/echa/code/iata"
+	"github.com/echa/code/iso"
 )
 
 type Metadata struct {
-	Address     tezos.Address `json:"address"`
-	Name        string        `json:"name"`
-	Category    string        `json:"category,omitempty"`
-	Status      string        `json:"status,omitempty"`
-	Country     string        `json:"country,omitempty"`
-	City        string        `json:"city,omitempty"`
-	Twitter     string        `json:"twitter,omitempty"`
-	HasLogo     bool          `json:"logo,omitempty"`
-	IsSponsored bool          `json:"sponsored,omitempty"`
+	// address + id together are used as unique identifier
+	Address tezos.Address `json:"address"`
+	AssetId int64         `json:"asset_id,omitempty"`
 
-	// baker info
-	Fee            float64         `json:"fee,omitempty"`
-	From           []tezos.Address `json:"from,omitempty"`
-	PayoutDelay    bool            `json:"payout_delay,omitempty"`
-	MinPayout      float64         `json:"min_payout,omitempty"`
-	MinDelegation  float64         `json:"min_delegation,omitempty"`
-	NonDelegatable bool            `json:"non_delegatable,omitempty"`
+	// public extensions
+	Alias    *AliasMetadata    `json:"alias,omitempty"`
+	Baker    *BakerMetadata    `json:"baker,omitempty"`
+	Payout   *PayoutMetadata   `json:"payout,omitempty"`
+	Asset    *AssetMetadata    `json:"token,omitempty"`
+	Location *LocationMetadata `json:"location,omitempty"`
+	Domain   *DomainMetadata   `json:"domain,omitempty"`
+	Media    *MediaMetadata    `json:"media,omitempty"`
+	Rights   *RightsMetadata   `json:"rights,omitempty"`
+	Social   *SocialMetadata   `json:"social,omitempty"`
+	Tz16     *Tz16Metadata     `json:"tz16,omitempty"`
+	Tz21     *Tz21Metadata     `json:"tz21,omitempty"`
 
-	// token info, type is custom, multisig, harbinger, dexter, fa12, tzbtc, staker
-	Standard string `json:"standard,omitempty"`
-	Code     string `json:"code,omitempty"`
-	Decimals int    `json:"decimals,omitempty"`
+	// private extensions
+	Extra map[string]interface{} `json:"-"`
+}
+
+func (m Metadata) MarshalJSON() ([]byte, error) {
+	type xMetadata Metadata
+	buf, err := json.Marshal(xMetadata(m))
+	if err != nil {
+		return nil, err
+	}
+	if len(m.Extra) == 0 {
+		return buf, nil
+	}
+	rev := make(map[string]interface{})
+	json.Unmarshal(buf, &rev)
+	for n, v := range m.Extra {
+		rev[n] = v
+	}
+	return json.Marshal(rev)
+}
+
+func (m *Metadata) UnmarshalJSON(buf []byte) error {
+	type xMetadata Metadata
+	xm := xMetadata{}
+	if err := json.Unmarshal(buf, &xm); err != nil {
+		return err
+	}
+	*m = Metadata(xm)
+	rev := make(map[string]interface{})
+	_ = json.Unmarshal(buf, &rev)
+	for n, v := range rev {
+		switch n {
+		case "address",
+			"asset_id",
+			"alias",
+			"baker",
+			"payout",
+			"token",
+			"location",
+			"domain",
+			"media",
+			"rights",
+			"social",
+			"tz16",
+			"tz21":
+			continue
+		default:
+			if m.Extra == nil {
+				m.Extra = make(map[string]interface{})
+			}
+			m.Extra[n] = v
+		}
+	}
+	return nil
+}
+
+type AliasMetadata struct {
+	Name        string `json:"name"`
+	Kind        string `json:"kind"`
+	Description string `json:"description,omitempty"`
+	Category    string `json:"category,omitempty"`
+	Logo        string `json:"logo,omitempty"`
+}
+
+type AssetMetadata struct {
+	Standard string   `json:"standard,omitempty"`
+	Symbol   string   `json:"symbol,omitempty"`
+	Decimals int      `json:"decimals,omitempty"`
+	Version  string   `json:"version,omitempty"`
+	Homepage string   `json:"homepage,omitempty"`
+	Genres   []string `json:"genres,omitempty"`
+	Tags     []string `json:"tags,omitempty"`
+}
+
+type BakerMetadata struct {
+	Status         string  `json:"status,omitempty"`
+	Fee            float64 `json:"fee,omitempty"`
+	PayoutDelay    bool    `json:"payout_delay,omitempty"`
+	MinPayout      float64 `json:"min_payout,omitempty"`
+	MinDelegation  float64 `json:"min_delegation,omitempty"`
+	NonDelegatable bool    `json:"non_delegatable,omitempty"`
+	Sponsored      bool    `json:"sponsored,omitempty"`
+}
+
+type PayoutMetadata struct {
+	From []tezos.Address `json:"from,omitempty"`
+}
+
+type LocationMetadata struct {
+	Country   iso.Country      `json:"country,omitempty"`
+	City      iata.AirportCode `json:"city,omitempty"`
+	Latitude  float64          `json:"lon,omitempty"`
+	Longitude float64          `json:"lat,omitempty"`
+	Altitude  float64          `json:"alt,omitempty"`
+}
+
+type DomainMetadata struct {
+	Address tezos.Address     `json:"address"`
+	Name    string            `json:"name"`
+	Owner   tezos.Address     `json:"owner"`
+	Expiry  time.Time         `json:"expiry"`
+	Data    map[string]string `json:"data,omitempty"`
+}
+
+type MediaMetadata struct {
+	ThumbnailUri string `json:"thumbnail_uri,omitempty"`
+	ArtifactUri  string `json:"artifact_uri,omitempty"`
+	Format       string `json:"format,omitempty"`
+	Language     string `json:"language,omitempty"`
+}
+
+type RightsMetadata struct {
+	Date         time.Time     `json:"date,omitempty"`
+	Rights       string        `json:"rights,omitempty"`
+	License      string        `json:"license,omitempty"`
+	Minter       tezos.Address `json:"minter,omitempty"`
+	Authors      []string      `json:"authors,omitempty"`
+	Creators     []string      `json:"creators,omitempty"`
+	Contributors []string      `json:"contributors,omitempty"`
+	Publishers   []string      `json:"publishers,omitempty"`
+}
+
+type SocialMetadata struct {
+	Twitter   string `json:"twitter,omitempty"`
+	Instagram string `json:"instagram,omitempty"`
+	Reddit    string `json:"reddit,omitempty"`
+	Github    string `json:"github,omitempty"`
+}
+
+type Tz16Metadata struct {
+	// TODO
+}
+
+type Tz21Metadata struct {
+	// TODO
 }
 
 func (c *Client) ListMetadata(ctx context.Context) ([]Metadata, error) {
@@ -42,9 +177,17 @@ func (c *Client) ListMetadata(ctx context.Context) ([]Metadata, error) {
 	return resp, nil
 }
 
-func (c *Client) GetMetadata(ctx context.Context, addr string) (Metadata, error) {
+func (c *Client) GetAccountMetadata(ctx context.Context, addr tezos.Address) (Metadata, error) {
 	var resp Metadata
-	if err := c.get(ctx, "/explorer/metadata/"+addr, nil, &resp); err != nil {
+	if err := c.get(ctx, "/explorer/metadata/"+addr.String(), nil, &resp); err != nil {
+		return resp, err
+	}
+	return resp, nil
+}
+
+func (c *Client) GetAssetMetadata(ctx context.Context, addr tezos.Address, assetId int64) (Metadata, error) {
+	var resp Metadata
+	if err := c.get(ctx, fmt.Sprintf("/explorer/metadata/%s/%d", addr, assetId), nil, &resp); err != nil {
 		return resp, err
 	}
 	return resp, nil
@@ -56,14 +199,14 @@ func (c *Client) CreateMetadata(ctx context.Context, metadata []Metadata) error 
 
 func (c *Client) UpdateMetadata(ctx context.Context, alias Metadata) (Metadata, error) {
 	var resp Metadata
-	if err := c.put(ctx, "/explorer/metadata/"+alias.Address.String(), nil, &alias, &resp); err != nil {
+	if err := c.put(ctx, fmt.Sprintf("/explorer/metadata/%s/%d", alias.Address, alias.AssetId), nil, &alias, &resp); err != nil {
 		return resp, err
 	}
 	return resp, nil
 }
 
-func (c *Client) RemoveMetadata(ctx context.Context, addr string) error {
-	return c.delete(ctx, "/explorer/metadata/"+addr, nil)
+func (c *Client) RemoveMetadata(ctx context.Context, addr tezos.Address, assetId int64) error {
+	return c.delete(ctx, fmt.Sprintf("/explorer/metadata/%s/%d", addr, assetId), nil)
 }
 
 func (c *Client) PurgeMetadata(ctx context.Context) error {
